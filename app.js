6c84f33387d2f8c8e063f9fb5c7d14ccac3c5b5e
Pramod <pramodraina047@gmail.com>
2022-10-29 17:10:38 +0000

    added js file

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const app = express();
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+const path = require("path");
+const dbPath = path.join(__dirname, "moviesData.db");
+
+let db = null;
+
+//initializing Database and Server
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server is running at http://localhost:3000/");
+    });
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
